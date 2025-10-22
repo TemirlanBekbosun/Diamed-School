2782ade1bff922e15a497ba4e4cad3a6f0761006
Bekmyrza200800 <bekmyrza986@gmail.com>
2025-10-22 13:24:22 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-// import MainPage from "./pages/MainPage";
+import MainPage from "./pages/MainPage";
 
-import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <>
-      <SignUp />
-      {/* <MainPage /> */}
+      <MainPage />
     </>
   );
 };
